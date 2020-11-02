a7538292bf462aca370c196a34c9a27ae655e884
gamil91 <alexis_gamil99@yahoo.com.com>
2020-11-02 15:44:31 -0800

    done

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,2 +1,10 @@
 class Employee < ApplicationRecord
+    belongs_to :dog
+    validates :alias, :title, uniqueness:true
+    validates :first_name, :last_name, :alias, :title, :office, presence:true
+
+    def full_name
+        self.first_name + " " + self.last_name
+    end
+
 end
